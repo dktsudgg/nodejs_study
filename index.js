84179ac85375a96118cfd4cab02014bf2c0f2600
dktsudgg <dktsudgg@yonsei.ac.kr>
2020-11-12 20:16:29 +0900

    테스트 코드 작성중

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
+var bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,9 @@ var users = [
 
 app.use(morgan('dev'));
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // 파라메타로 받은 이 두 객체는 express가 http모듈의 req, res를 래핑한 객체임.
 app.get('/users', (req, res) => {
   req.query.limit = req.query.limit || 10;      // 바로 밑에서 limit값 숫자로 파싱 실패 시 기본값 10으로 설정하기 위함..
@@ -22,6 +26,44 @@ app.get('/users', (req, res) => {
     return res.status(400).end();
   }
   res.json(users.slice(0, limit));  // json형태로 응답
+});
+
+app.get('/users/:id', function(req, res){
+  const id = parseInt(req.params.id, 10);
+  if(Number.isNaN(id))  return res.status(400).end();
+
+  const user = users.filter((user) => user.id === id)[0];
+  if(!user) return res.status(404).end();
+
+  res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if(Number.isNaN(id))  return res.status(400).end();
+
+  users = users.filter((user) => user.id !== id);
+  res.status(204).end();
+});
+
+app.post('/users', (req, res)=>{
+  /**
+   * express는 body를 지원하지 않기 때문에 추가적인 모듈을 설치해야 한다. body-parser와 multer... multer는 이미지같은 큰 데이터를 사용할 때 사용..
+   * npm i body-parser --save
+   * 윗부분에 app.use~~ 하는 코드 추가.. http://expressjs.com/en/resources/middleware/body-parser.html
+   */
+  const name = req.body.name;
+  if(!name) return res.status(400).end();
+
+  // name 중복 체크
+  const isConflic = users.filter((user)=> user.name === name).length;
+  if(isConflic) return res.status(409).end();
+
+  const id = Date.now();
+  const user = {id, name};
+  users.push(user);
+  res.status(201).json(user);
+
 });
 
 app.listen(port, () => {
--- index.spec.js
+++ index.spec.js
@@ -35,3 +35,91 @@ describe('GET /users는', () => {
         });
     });
 });
+
+describe('GET /users/1는', () => {
+    describe('성공 시,', () => {
+        it('id가 1인 유저 객체를 반환한다.', (done) => {
+            request(app)
+                .get('/users/1')
+                .end((err, res) => {
+                    res.body.should.have.property('id', 1);
+                    done();
+                });
+        });
+    });
+    describe('실패 시,', () => {
+        it('id가 숫자가 아닐 경우 400으로 응답한다.', (done) => {
+            request(app)
+                .get('/users/one')
+                .expect(400)
+                .end(done);
+        });
+        it('id로 유저를 찾을 수 없을 경우 404으로 응답한다.', (done) => {
+            request(app)
+                .get('/users/999')
+                .expect(404)
+                .end(done);
+        });
+    })
+});
+
+describe('DELETE /users/1는', () => {
+    describe('성공 시', () => {
+        it('204를 응답한다', (done) => {
+            request(app)
+                .delete('/users/1')
+                .expect(204)
+                .end(done);
+        })
+    });
+    describe('실패 시', () => {
+        it('id가 숫자가 아닐 경우 400으로 응답한다', (done) => {
+            request(app)
+                .delete('/users/one')
+                .expect(400)
+                .end(done);
+        })
+    });
+})
+
+describe('POST /users는', () => {
+    describe('성공 시', ()=>{
+        let body;
+        let name = 'daniel';
+        before(done=>{
+            request(app)
+                .post('/users')
+                .send({name}) //{name: name} 이랑 같다
+                .expect(201)
+                .end((err, res)=>{
+                    body = res.body;
+                    done();
+                })
+        });
+
+        it('생성된 유저 객체를 반환한다', ()=>{
+            body.should.have.property('id');
+        });
+        it('입력한 name을 반환한다.', ()=>{
+            body.should.have.property('name', name);
+        })
+    })
+    describe('실패 시', ()=>{
+
+        it('name 파라미터 누락 시 400을 반환한다', (done)=>{
+            request(app)
+                .post('/users')
+                .send({})
+                .expect(400)
+                .end(done)
+        });
+        it('name이 중복일 경우 409를 반환한다', (done)=>{
+            request(app)
+                .post('/users')
+                .send({name: 'daniel'}) // 이미 있는 데이터를 사용하여 테스트
+                .expect(409)
+                .end(done)
+        })
+    })
+    
+})
